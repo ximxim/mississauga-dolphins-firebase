453a35a806d0a3d35bd1b931691b36e08f237c91
Azim Ahmed <azim.ahmed7@gmail.com>
2018-04-06 09:52:53 -0400

    updated instagram code to use self acccess token

--- functions/instagramFeed.js
+++ functions/instagramFeed.js
@@ -18,7 +18,7 @@ module.exports = async((config) => {
 
 const requestIntagram = (igUserId, baseUrl, accessToken) =>
     new Promise((resolve) =>
-        request(`${baseUrl}/users/${igUserId}/media/recent/?access_token=${accessToken}&count=200`,
+        request(`${baseUrl}/users/self/media/recent/?access_token=${accessToken}&count=200`,
             (error, response, body) => (error) ? resolve(error) : resolve(body))
     );
 
