aba9606b9ddaefa2d2c777a5791c492029c919ef
Azim Ahmed <azim.ahmed7@gmail.com>
2018-07-08 21:37:53 -0400

    added push register function

--- functions/firebaseRequests.js
+++ functions/firebaseRequests.js
@@ -2,11 +2,11 @@ const async = require('es5-async-await/async');
 const _ = require('lodash');
 const await = require('es5-async-await/await');
 const serviceAccount = require('./md-firebase-admin.json');
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://mississauga-dolphins.firebaseio.com",
+    databaseURL: 'https://mississauga-dolphins.firebaseio.com'
 });
 
 let db = admin.database();
@@ -15,33 +15,33 @@ let ref = db.ref('/');
 function FirebaseRequests() {
     db = admin.database();
     ref = db.ref('/');
-};
+}
 
-FirebaseRequests.prototype.getObjectByName = async((name) => {
+FirebaseRequests.prototype.getObjectByName = async(name => {
     const newsFeedRef = ref.child(name);
-    return new Promise((resolve) =>
-        newsFeedRef.once('value', (snapshot) => {
+    return new Promise(resolve =>
+        newsFeedRef.once('value', snapshot => {
             resolve(snapshot.val());
         })
     );
 });
 
-FirebaseRequests.prototype.getNewsFeedByCount = async((count) => {
+FirebaseRequests.prototype.getNewsFeedByCount = async(count => {
     const newsFeedRef = ref.child('NewsFeed');
-    return new Promise((resolve) =>
+    return new Promise(resolve =>
         newsFeedRef
             .orderByChild('date')
             .limitToLast(count)
-            .once('value', (snapshot) => {
+            .once('value', snapshot => {
                 resolve(snapshot.val());
             })
     );
 });
 
-FirebaseRequests.prototype.getNewsFeedItemById = async((id) => {
+FirebaseRequests.prototype.getNewsFeedItemById = async(id => {
     const itemReference = ref.child(`NewsFeed/${id}`);
-    return new Promise((resolve) =>
-        itemReference.once('value', (snapshot) => {
+    return new Promise(resolve =>
+        itemReference.once('value', snapshot => {
             resolve(snapshot.val());
         })
     );
@@ -49,18 +49,33 @@ FirebaseRequests.prototype.getNewsFeedItemById = async((id) => {
 
 FirebaseRequests.prototype.updateItemById = async((reference, id, item) => {
     if (reference && id && item) {
-        ref.child(reference).child(id).update(item);
+        ref.child(reference)
+            .child(id)
+            .update(item);
     }
 });
 
-FirebaseRequests.prototype.removeNewsFeedItemsByIdPrefix = async((prefix) => {
-    const newsFeedRef = ref.child('NewsFeed');
-    const newsFeed = await(new Promise((resolve) => newsFeedRef.once('value', (snapshot) => resolve(snapshot.val()))));
-    _.map(newsFeed, (item) => (item.id.indexOf(prefix) > - 1) ? delete newsFeed[item.id] : null)
-    return new Promise((resolve) => newsFeedRef.set(newsFeed, () => resolve()));
+FirebaseRequests.prototype.addItemByNode = async((reference, item) => {
+    if (reference && item) {
+        ref.child(reference).set(item);
+    }
 });
 
-FirebaseRequests.prototype.removeByRef = async((r) => {
+FirebaseRequests.prototype.removeNewsFeedItemsByIdPrefix = async(prefix => {
+    const newsFeedRef = ref.child('NewsFeed');
+    const newsFeed = await(
+        new Promise(resolve =>
+            newsFeedRef.once('value', snapshot => resolve(snapshot.val()))
+        )
+    );
+    _.map(
+        newsFeed,
+        item => (item.id.indexOf(prefix) > -1 ? delete newsFeed[item.id] : null)
+    );
+    return new Promise(resolve => newsFeedRef.set(newsFeed, () => resolve()));
+});
+
+FirebaseRequests.prototype.removeByRef = async(r => {
     const reference = ref.child(r);
     return reference.remove();
 });
