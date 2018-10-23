4f51d968dcc576d4037aded565802a3cae421ac2
Azim Ahmed <azim.ahmed7@gmail.com>
2018-10-22 23:43:11 -0400

    added push notification logic to test trigger

--- functions/index.js
+++ functions/index.js
@@ -323,29 +323,87 @@ exports.testPushNotification = functions.https.onRequest(
 exports.triggerWriteTest = functions.database
     .ref('/Test')
     .onWrite(async((change, context) => {
+        let expo = new Expo();
+        let messages = [];
+        let pushUsers = [];
+        let body = '';
         const before = change.before.val();
         const after = change.after.val();
+        const users = await(firebaseRequests.getObjectByName('Users'));
+        const key = Object.keys(before)[0];
+        const score = `${after[key].home.name}: ${after[key].home.score}/${after[key].home.wickets} vs ${after[key].visitor.name}: ${after[key].visitor.score}/${after[key].visitor.wickets}`;
 
-        if (before === null && after !== null) {
-            await(slackMessages.message('started game'));
-        } else if (before !== null && after === null) {
-            await(slackMessages.message('ended game'));
+        if (!before[key].active && !after[key].active) return;
+
+        if (!before && after) {
+            // GAME STARTED
+            pushUsers = _.filter(users,
+                user => user.notification_settings.game_start === 'true');
+
+            body = `A game started between ${after.home.name} vs ${after.visitor.name}. Checkout the live scores in app.`;
+        } else if (before && !after) {
+            // GAME ENDED
+            pushUsers = _.filter(users,
+                user => user.notification_settings.game_ends === 'true');
+
+            body = `A game ended between ${score}`;
         } else if (before && after) {
-            const key = Object.keys(before)[0];
             if (!before[key].active && after[key].active) {
-                await(slackMessages.message('started game'));
+                // GAME STARTED
+                pushUsers = _.filter(users,
+                    user => user.notification_settings.game_start === 'true');
+
+                body = `A game started between ${after[key].home.name} vs ${after[key].visitor.name}. Checkout the live scores in app.`;
             } else if (before[key].active && !after[key].active) {
-                await(slackMessages.message('ended game'));
+                // GAME ENDED
+                pushUsers = _.filter(users,
+                    user => user.notification_settings.game_ends === 'true');
+
+                body = `A game ended between ${score}.`;
             } else if ((before[key].home.batting
                 && !after[key].home.batting)
                 || (before[key].visitor.batting
                 && !after[key].visitor.batting)) {
-                await(slackMessages.message('batting stance changed'));
+                    // BATTING STANCE CHANGED
+                pushUsers = _.filter(users,
+                    user => user.notification_settings.positions_change === 'true');
+
+                const battingTeam = after[key].home.batting === 'true' ? after[key].home.name : after[key].visitor.name;
+                body = `${battingTeam} is batting now. Score is ${score}`
             } else if ((before[key].home.wickets !== after[key].home.wickets)
                 || (before[key].visitor.wickets !== after[key].visitor.wickets)) {
-                await(slackMessages.message('Wickets changed'));
+                    // WICKETS CHANGED
+                    pushUsers = _.filter(users,
+                        user => user.notification_settings.score_wicket === 'true');
+
+                    const wicketTeam = before[key].home.wickets !== after[key].home.wickets ? after[key].home.name : after[key].visitor.name;
+
+                    body = `${wicketTeam} lost a wicket. Score is ${score}`;
             }
         }
+
+        _.map(pushUsers, user => {
+            if (!Expo.isExpoPushToken(user.token)) {
+                console.log('token is invalid');
+            } else {
+                messages.push({
+                    to: user.token,
+                    sound: 'default',
+                    title: 'Mississauga Dolphins',
+                    body,
+                });
+            }
+        });
+
+        let chunks = expo.chunkPushNotifications(messages);
+        _.map(chunks, chunk => {
+            try {
+                let receipts = await(expo.sendPushNotificationsAsync(chunk));
+                console.log(receipts);
+            } catch (error) {
+                console.error(error);
+            }
+        });
     }));
 
 // exports.triggerUpdateTest = functions.database
