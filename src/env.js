874fedeb78d70aa47f3a9bc8dd38cd8d3eb2a3e0
Azim Ahmed <azim.ahmed7@gmail.com>
2018-11-09 23:31:36 -0500

    added staging instance and environment variables to firebase hosting

--- src/env.js
+++ src/env.js
@@ -1,5 +1,15 @@
 import develop from './env.develop';
 import production from './env.production';
+import staging from './env.staging';
 
-const buildConfigs = { development: develop, production };
-export default buildConfigs[process.env.NODE_ENV];
+const buildConfigs = { develop, staging, production };
+let environment;
+
+if (process.env.NODE_ENV === 'development') {
+  environment = 'develop';
+} else if (window.location.hostname.indexOf('staging') !== -1) {
+  environment = 'staging';
+} else {
+  environment = 'production';
+}
+export default buildConfigs[environment];
---
+++ src/env.staging.js
@@ -0,0 +1,33 @@
+export default {
+  firebase: {
+    apiKey: 'AIzaSyCPu1cq_G_tzSvrnmKMV02g8yES4j_5kIg',
+    authDomain: 'mississauga-dolphins.firebaseapp.com',
+    databaseURL: 'https://mississauga-dolphins-staging.firebaseio.com',
+    projectId: 'mississauga-dolphins',
+    storageBucket: 'mississauga-dolphins.appspot.com',
+    messagingSenderId: '562485347443',
+  },
+  newGame: {
+    home: {
+      name: 'Pakistan',
+      score: 200,
+      wickets: 5,
+      overs: 4,
+      batting: true,
+    },
+    visitor: {
+      name: 'India',
+      score: 100,
+      wickets: 8,
+      overs: 9,
+      batting: false,
+    },
+    striker: 'John Smith',
+    nonStriker: 'Azim Ahmed',
+    bowler: 'Adeel Ahmed',
+  },
+  login: {
+    username: 'azim.ahmed7@gmail.com',
+    password: '826474',
+  },
+};
