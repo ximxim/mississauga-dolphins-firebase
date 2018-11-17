f5b649922a6ea9bd656566f988418db53039a24f
Azim Ahmed <azim.ahmed7@gmail.com>
2018-11-16 23:10:54 -0500

    minor eslint tweaks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* @flow */
 import React, { Component } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
@@ -7,22 +8,27 @@ import withSetup from './js/redux/setup';
 import games from './js/routes/games';
 import game from './js/routes/game';
 
-class App extends Component {
+type Props = {
+    location: {
+        pathname: string,
+    },
+}
 
-    render() {
-        if (this.props.location.pathname === '/') {
-            return (<Redirect to={'/games'} />);
-        }
-        return (
-            <div>
-                <React.Fragment>
-                    <Route path="/games" component={games} />
-                    <Route path="/game/:id" component={game} />
-                </React.Fragment>
-                <ToastContainer />
-            </div>
-        );
+class App extends Component<Props, *> {
+  render() {
+    if (this.props.location.pathname === '/') {
+      return (<Redirect to="/games" />);
     }
+    return (
+        <div>
+            <React.Fragment>
+                <Route path="/games" component={games} />
+                <Route path="/game/:id" component={game} />
+            </React.Fragment>
+            <ToastContainer />
+        </div>
+    );
+  }
 }
 
 export default withSetup(App);
