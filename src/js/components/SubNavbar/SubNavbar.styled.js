a34da75275ef4974851f4a7ec13a857a4c05a704
Azim Ahmed <azim.ahmed7@gmail.com>
2018-12-01 23:39:38 -0500

    styled navbar buttons for mobile

--- src/js/components/SubNavbar/SubNavbar.styled.js
+++ src/js/components/SubNavbar/SubNavbar.styled.js
@@ -2,6 +2,29 @@ import styled from 'styled-components';
 import { media } from '../../utils/media';
 
 export const SubNavBarButton = styled.button`
+display: flex;
+justify-content: center;
+align-items: center;
+border: 0px;
+border-top: ${props => `1px solid ${props.theme.darkGrey}`};
+margin-top: 5px;
+background-color: ${props => props.theme.white}
+${media.medium`
+color: ${props => props.theme.brandPrimary};
+border: ${props => `2px solid ${props.theme.brandPrimary}`};
+border-radius: 10px;
+background-color: ${props => props.theme.white};
+cursor: pointer;
+transition: all 0.3s;
+margin-left: 5px;
+&:hover {
+    background-color: ${props => props.theme.brandPrimary};
+    color: ${props => props.theme.white};
+}
+`}
+`;
+
+export const SubToggleButton = styled.button`
 color: ${props => props.theme.brandPrimary};
 border: ${props => `2px solid ${props.theme.brandPrimary}`};
 border-radius: 10px;
@@ -16,9 +39,6 @@ margin-left: 5px;
     background-color: ${props => props.theme.brandPrimary};
     color: ${props => props.theme.white};
 }
-`;
-
-export const SubToggleButton = styled(SubNavBarButton)`
 ${media.medium`
 display: none;
 `}
