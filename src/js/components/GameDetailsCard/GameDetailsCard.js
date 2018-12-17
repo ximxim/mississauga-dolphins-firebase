eefca7f104788f1a92499d07bec5cb94a230ca64
Azim Ahmed <azim.ahmed7@gmail.com>
2018-12-16 19:40:19 -0500

    refactored gamescorecard

--- src/js/components/GameDetailsCard/GameDetailsCard.js
+++ src/js/components/GameDetailsCard/GameDetailsCard.js
@@ -10,46 +10,46 @@ type Props = {
 };
 
 export default class GameDetailsCard extends React.Component<Props, *> {
-  render() {
-	  const { game } = this.props;
-    return (
-        <div className="card m-1 border-2 rounded overflow-hidden">
-            <div className="pb-2">
-                <div className="position-relative text-center text-white">
-                    <img
-                        src={game.cover.source}
-                        alt="game cover"
-                        className="w-100"
-                    />
+    render() {
+        const { game } = this.props;
+        return (
+            <div className="card m-1 border-2 rounded overflow-hidden">
+                <div className="pb-2">
+                    <div className="position-relative text-center text-white">
+                        <img
+                            src={game.cover.source}
+                            alt="game cover"
+                            className="w-100"
+                        />
+                    </div>
+                    <Table className="mb-0" striped>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <FontAwesome.FaClockO
+                                        className="align-self-center fs-6"
+                                    />
+                                </td>
+                                <td style={{ textAlign: 'left' }}>
+                                    {moment(game.start_time).format(
+                                        'MMMM Do YYYY, h:mm: a',
+                                    )}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <FontAwesome.FaInfoCircle
+                                        className="align-self-center fs-6"
+                                    />
+                                </td>
+                                <td style={{ textAlign: 'left' }}>
+                                    <p>{game.match_no}</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </div>
-                <Table className="mb-0" striped>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <FontAwesome.FaClockO
-                                    className="align-self-center fs-6"
-                                />
-                            </td>
-                            <td style={{ textAlign: 'left' }}>
-                                {moment(game.start_time).format(
-									  'MMMM Do YYYY, h:mm: a',
-                                )}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <FontAwesome.FaInfoCircle
-                                    className="align-self-center fs-6"
-                                />
-                            </td>
-                            <td style={{ textAlign: 'left' }}>
-                                <p>{game.match_no}</p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </Table>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
