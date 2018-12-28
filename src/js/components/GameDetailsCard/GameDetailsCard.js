2e90167c605fb08565686a12bc22fedd7ffc8bf4
Azim Ahmed <azim.ahmed7@gmail.com>
2018-12-27 19:11:16 -0500

    small fixes

--- src/js/components/GameDetailsCard/GameDetailsCard.js
+++ src/js/components/GameDetailsCard/GameDetailsCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as FontAwesome from 'react-icons/lib/fa';
 import moment from 'moment';
 
 import { Game } from '../../redux/modules/Scores/types';
@@ -31,44 +30,5 @@ export default class GameDetailsCard extends React.Component<Props, *> {
                 </div>
             </div>
         );
-        // return (
-        // <div className="card m-1 border-2 rounded overflow-hidden">
-        //     <div className="pb-2">
-        //         <div className="position-relative text-center text-white">
-        //             <img
-        //                 src={game.cover.source}
-        //                 alt="game cover"
-        //                 className="w-100"
-        //             />
-        //         </div>
-        //         <Table className="mb-0">
-        //             <tbody>
-        //                 <tr>
-        //                     <td>
-        //                         <FontAwesome.FaClockO
-        //                             className="align-self-center fs-6"
-        //                         />
-        //                     </td>
-        //                     <td style={{ textAlign: 'left' }}>
-        //                         {moment(game.start_time).format(
-        //                             'MMMM Do YYYY, h:mm: a',
-        //                         )}
-        //                     </td>
-        //                 </tr>
-        //                 <tr>
-        //                     <td>
-        //                         <FontAwesome.FaInfoCircle
-        //                             className="align-self-center fs-6"
-        //                         />
-        //                     </td>
-        //                     <td style={{ textAlign: 'left' }}>
-        //                         <p>{game.match_no}</p>
-        //                     </td>
-        //                 </tr>
-        //             </tbody>
-        //         </Table>
-        //     </div>
-        // </div>
-        // );
     }
 }
