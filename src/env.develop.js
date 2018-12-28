b54336b9920659e11ec650cbfee999afc67a92ac
Azim Ahmed <azim.ahmed7@gmail.com>
2018-12-28 13:57:08 -0500

    created production build and deployed

--- src/env.develop.js
+++ src/env.develop.js
@@ -1,33 +1,33 @@
 export default {
-  firebase: {
-    apiKey: 'AIzaSyCPu1cq_G_tzSvrnmKMV02g8yES4j_5kIg',
-    authDomain: 'mississauga-dolphins.firebaseapp.com',
-    databaseURL: 'https://mississauga-dolphins-dev.firebaseio.com',
-    projectId: 'mississauga-dolphins',
-    storageBucket: 'mississauga-dolphins.appspot.com',
-    messagingSenderId: '562485347443',
-  },
-  newGame: {
-    home: {
-      name: 'Pakistan',
-      score: 200,
-      wickets: 5,
-      overs: 4,
-      batting: true,
+    firebase: {
+        apiKey: 'AIzaSyCPu1cq_G_tzSvrnmKMV02g8yES4j_5kIg',
+        authDomain: 'mississauga-dolphins.firebaseapp.com',
+        databaseURL: 'https://mississauga-dolphins-dev.firebaseio.com',
+        projectId: 'mississauga-dolphins',
+        storageBucket: 'mississauga-dolphins.appspot.com',
+        messagingSenderId: '562485347443',
     },
-    visitor: {
-      name: 'India',
-      score: 100,
-      wickets: 8,
-      overs: 9,
-      batting: false,
+    newGame: {
+        home: {
+            name: 'Pakistan',
+            score: 200,
+            wickets: 5,
+            overs: 4,
+            batting: true,
+        },
+        visitor: {
+            name: 'India',
+            score: 100,
+            wickets: 8,
+            overs: 9,
+            batting: false,
+        },
+        striker: 'John Smith',
+        nonStriker: 'Azim Ahmed',
+        bowler: 'Adeel Ahmed',
     },
-    striker: 'John Smith',
-    nonStriker: 'Azim Ahmed',
-    bowler: 'Adeel Ahmed',
-  },
-  login: {
-    username: 'azim.ahmed7@gmail.com',
-    password: '826474',
-  },
+    login: {
+        username: 'azim.ahmed7@gmail.com',
+        password: '826474',
+    },
 };
--- src/env.staging.js
+++ src/env.staging.js
@@ -1,33 +1,33 @@
 export default {
-  firebase: {
-    apiKey: 'AIzaSyCPu1cq_G_tzSvrnmKMV02g8yES4j_5kIg',
-    authDomain: 'mississauga-dolphins.firebaseapp.com',
-    databaseURL: 'https://mississauga-dolphins-staging.firebaseio.com',
-    projectId: 'mississauga-dolphins',
-    storageBucket: 'mississauga-dolphins.appspot.com',
-    messagingSenderId: '562485347443',
-  },
-  newGame: {
-    home: {
-      name: '',
-      score: 0,
-      wickets: 0,
-      overs: 0,
-      batting: true,
+    firebase: {
+        apiKey: 'AIzaSyCPu1cq_G_tzSvrnmKMV02g8yES4j_5kIg',
+        authDomain: 'mississauga-dolphins.firebaseapp.com',
+        databaseURL: 'https://mississauga-dolphins-staging.firebaseio.com',
+        projectId: 'mississauga-dolphins',
+        storageBucket: 'mississauga-dolphins.appspot.com',
+        messagingSenderId: '562485347443',
     },
-    visitor: {
-      name: '',
-      score: 0,
-      wickets: 0,
-      overs: 0,
-      batting: false,
+    newGame: {
+        home: {
+            name: '',
+            score: 0,
+            wickets: 0,
+            overs: 0,
+            batting: true,
+        },
+        visitor: {
+            name: '',
+            score: 0,
+            wickets: 0,
+            overs: 0,
+            batting: false,
+        },
+        striker: '',
+        nonStriker: '',
+        bowler: '',
     },
-    striker: '',
-    nonStriker: '',
-    bowler: '',
-  },
-  login: {
-    username: '',
-    password: '',
-  },
+    login: {
+        username: '',
+        password: '',
+    },
 };
